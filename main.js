6e29004345987478cbb13c3b1aed9d750bd6c945
Mohammed-Sebaa <mmohamedsebaa@gmail.com>
2019-09-19 11:13:26 +0200

    Slider animation ajoutées

--- main.js
+++ main.js
@@ -1,30 +1,15 @@
-/*const navBar = document.getElementsByTagName('nav')
-const navSlide = () => {
-    const logButton = document.getElementById('log-button');
-    const logSlider = document.getElementById('log-slider');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    //active la nav au click
-    logButton.addEventListener('click', () => {
-        logSlider.classList.toggle('slider-active');
-    });
-}
-navSlide(); */
-
-/* function loginButton() {
-    const logSlider = document.querySelector('log-slider');
+function signButton() {
+    const logSlider = document.querySelector('.sign-slider');
     logSlider.classList.toggle('slider-active');
-
-} */
-
-function navSlide() {
-    const logButton = document.getElementById('log-button');
-    const logSlider = document.getElementById('log-slider');
-
-    console.log(logSlider);
-    logButton.addEventListener('click', () => {
-        logSlider.classList.toggle('slider-active');
-    });
 }
 
-navSlide();
+function loginButton() {
+    const logSlider = document.querySelector('.log-slider');
+    logSlider.classList.toggle('slider-active');
+} 
+
+function profileButton() {
+    const logSlider = document.querySelector('.profile-slider');
+    logSlider.classList.toggle('slider-active');
+} 
+
