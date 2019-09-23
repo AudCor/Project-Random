5b02a243ad7f217159165fe3b3d4f9a870b2800f
AudCor <clapyourhands4ever@gmail.com>
2019-09-23 10:18:20 +0200

    amélioration des fontions signButton(), loginButton() et profileButton()

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
+const signSlider = document.querySelector('.sign-slider');
+const logSlider = document.querySelector('.log-slider');
+const profileSlider = document.querySelector('.profile-slider');
+console.log(signSlider);
+
 function signButton() {
-    const signSlider = document.querySelector('.sign-slider');
+  if (logSlider.classList.contains('slider-active'))
+  {
+    logSlider.classList.replace('slider-active', 'slider-closing');
+    // fonction setTimeOut
+    signSlider.classList.toggle('slider-active'); // mettre un delay
+    signSlider.classList.toggle('slider-closing');
+  } else if (profileSlider.classList.contains('slider-active'))
+  {
+    profileSlider.classList.replace('slider-active', 'slider-closing');
     signSlider.classList.toggle('slider-active');
     signSlider.classList.toggle('slider-closing');
-
+  } else
+  {
+    signSlider.classList.toggle('slider-active');
+    signSlider.classList.toggle('slider-closing');
+  }
 }
 
 function loginButton() {
-    const logSlider = document.querySelector('.log-slider');
+  if (signSlider.classList.contains('slider-active'))
+  {
+    signSlider.classList.replace('slider-active', 'slider-closing');
     logSlider.classList.toggle('slider-active');
-    logSlider.classList.toggle('slider-closing');
-} 
+    logSlider.classList.toggle('slider-closing');  
+  } else if (profileSlider.classList.contains('slider-active'))
+  {
+    profileSlider.classList.replace('slider-active', 'slider-closing');
+    logSlider.classList.toggle('slider-active');
+    logSlider.classList.toggle('slider-closing');  
+  } else
+  {
+    logSlider.classList.toggle('slider-active');
+    logSlider.classList.toggle('slider-closing');  
+  }
+}
+
 
 function profileButton() {
-    const profileSlider = document.querySelector('.profile-slider');
+  if (signSlider.classList.contains('slider-active'))
+  {
+    signSlider.classList.replace('slider-active', 'slider-closing');
     profileSlider.classList.toggle('slider-active');
-    profileSlider.classList.toggle('slider-closing');
-
-} 
+    profileSlider.classList.toggle('slider-closing');  
+  } else if (logSlider.classList.contains('slider-active'))
+  {
+    logSlider.classList.replace('slider-active', 'slider-closing');
+    profileSlider.classList.toggle('slider-active');
+    profileSlider.classList.toggle('slider-closing');  
+  } else
+  { 
+  profileSlider.classList.toggle('slider-active');
+  profileSlider.classList.toggle('slider-closing');
+  } 
+}
 
 // function chooseResult() {
 //     const nameDiv = document.querySelectorAll('.all-divs');
