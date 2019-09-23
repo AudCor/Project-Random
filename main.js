1153882ba346b1704400336a9174867f276d45d4
Mohammed-Sebaa <mmohamedsebaa@gmail.com>
2019-09-23 10:17:05 +0200

    Ajout de font, arriere plan grisé

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
 function signButton() {
     const signSlider = document.querySelector('.sign-slider');
+    const bgDark = document.querySelector('.dark-bg');
     signSlider.classList.toggle('slider-active');
     signSlider.classList.toggle('slider-closing');
-
+    bgDark.classList.toggle('dark-bg-active')
 }
 
 function loginButton() {
     const logSlider = document.querySelector('.log-slider');
+    const bgDark = document.querySelector('.dark-bg');
     logSlider.classList.toggle('slider-active');
     logSlider.classList.toggle('slider-closing');
+    bgDark.classList.toggle('dark-bg-active')
 } 
 
 function profileButton() {
     const profileSlider = document.querySelector('.profile-slider');
+    const bgDark = document.querySelector('.dark-bg');
     profileSlider.classList.toggle('slider-active');
     profileSlider.classList.toggle('slider-closing');
-
+    bgDark.classList.toggle('dark-bg-active')
 } 
-
-// function chooseResult() {
-//     const nameDiv = document.querySelectorAll('.all-divs');
-//     let addingTime = 0;
-//     switch () {
-//         case nameDiv[0]:
-            
-//             break;
-    
-//         default:
-//             break;
-//     }
-// }
